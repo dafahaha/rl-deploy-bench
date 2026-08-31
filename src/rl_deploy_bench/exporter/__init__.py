94e2c11d43f60e2af5844bc8814d69843515d217
dafahaha <156556011+dafahaha@users.noreply.github.com>
2026-08-31 23:24:32 +0800

    Add TorchScript export utilities to __init__.py

--- src/rl_deploy_bench/exporter/__init__.py
+++ src/rl_deploy_bench/exporter/__init__.py
@@ -2,14 +2,26 @@
 
 from .onnx_export import ExportConfig, OnnxablePolicy, export_to_onnx, verify_onnx_export
 from .sb3 import SB3OnnxablePolicy, export_sb3_model, load_sb3_model, verify_sb3_export
+from .torchscript_export import (
+    TorchScriptConfig,
+    TorchScriptInference,
+    compare_onnx_torchscript,
+    export_to_torchscript,
+    verify_torchscript_export,
+)
 
 __all__ = [
     "ExportConfig",
     "OnnxablePolicy",
-    "export_to_onnx",
-    "verify_onnx_export",
     "SB3OnnxablePolicy",
+    "TorchScriptConfig",
+    "TorchScriptInference",
+    "compare_onnx_torchscript",
     "export_sb3_model",
+    "export_to_onnx",
+    "export_to_torchscript",
     "load_sb3_model",
+    "verify_onnx_export",
     "verify_sb3_export",
+    "verify_torchscript_export",
 ]
